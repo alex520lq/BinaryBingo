cbf31c29af94ea5a6224996aac995075c8e25dd0
Michael Manley <mmanle01@gmail.com>
2017-03-11 15:07:16 -0500

    Addition of my Bingo Board class from another project and changes to fit the Unity framework. Added game manager skeleton

---
+++ Assets/Scripts/BingoBoard.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class BingoBoard : MonoBehaviour {
+
+    private int[][] board;
+
+    public int[][] Board { get { return board; } }
+
+    public BingoBoard()
+    {
+        board = new int[5][];
+        generateNewBoard(board);
+    }
+
+    public int FindValue(int key, Column col)
+    {
+        //Get min and max values for the column and then pass it in.
+        int min = board[(int)col].Min();
+        int max = board[(int)col].Max();
+        return binarysearchrecursive(board[(int)col], key, min, max);
+    }
+
+    private static int binarysearchrecursive(int[] inputarray, int key, int min, int max)
+    {
+        if (max < min) { return -1; }
+
+        int mid = (min + max) / 2;
+        if (key == inputarray[mid])
+        {
+            return mid;
+        }
+        else if (key < inputarray[mid])
+        {
+            return binarysearchrecursive(inputarray, key, min, mid - 1);
+        }
+        else
+        {
+            return binarysearchrecursive(inputarray, key, mid + 1, max);
+        }
+    }
+
+    private void generateNewBoard(int[][] board)
+    {
+        int value = 1;
+        for (int column = 0; column < board.Length; column++)
+        {
+            board[column] = new int[5];
+            for (int row = 0; row < 5; row++)
+            {
+                int addedRandomNumber = Random.Range(1, 19);
+                if (value + addedRandomNumber < 100)
+                {
+                    value += addedRandomNumber;
+                    int newValue = value;
+                    board[column][row] = newValue;
+                }
+
+            }
+            value = 1;//Reset per column
+        }
+    }
+
+    public enum Column
+    {
+        B, I, N, G, O
+    }
+}
